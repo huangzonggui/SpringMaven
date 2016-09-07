3088da343ba378a405735f3406c7664854b30a2b
huangagui <1226620085@qq.com>
2016-09-08 00:30:43 +0800

    Initial commit

---
+++ src/main/java/com/imooc/ioc/injection/service/InjectionServiceImpl.java
@@ -0,0 +1,23 @@
+package com.imooc.ioc.injection.service;
+
+import com.imooc.ioc.injection.dao.InjectionDAO;
+
+public class InjectionServiceImpl implements InjectionService{
+
+	private InjectionDAO injectionDAO;
+	
+	//设值注入
+	public void setInjectionDAO(InjectionDAO injectionDAO) {
+		this.injectionDAO = injectionDAO;
+	}
+
+	@Override
+	public void save(String arg) {
+		//模拟业务操作
+		System.out.println("Service接收参数："+arg);
+		arg=arg+":"+this.hashCode();
+		injectionDAO.save(arg);
+	}
+	
+
+}
